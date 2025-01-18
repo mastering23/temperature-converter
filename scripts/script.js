d33edfaa4f697a63968b769c426608b97e43fb24
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-01-18 01:48:41 -0500

    adding functions for user prompt to get data | also calculations for convert fah to celsius | finally adding if and else if condictions |

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+const userPrompt = () => {
+  const userInput = prompt(`Enter Fahrenheit temperature value : `);
+
+  return Number(userInput);
+};
+
+const convertToCelsius = () => {
+  const FahrenheitValue = userPrompt();
+
+  const celsiusTemp = (FahrenheitValue - 32) * (5 / 9);
+
+  return Math.floor(celsiusTemp);
+};
+
+const describeTemperature = () => {
+  const celsiusValue = convertToCelsius();
+
+  if (celsiusValue < 32) {
+    let message = `Today Temperature is ${celsiusValue} a very cold day!`;
+  } else if (celsiusValue < 64) {
+    message = `Today Temperature is ${celsiusValue} a cold day!`;
+  } else if (celsiusValue < 86) {
+    message = `Today Temperature is ${celsiusValue} a warm day!`;
+  } else if (celsiusValue < 100) {
+    message = `Today Temperature is ${celsiusValue} a hot day!`;
+  } else if (celsiusValue >= 100) {
+    message = `Today Temperature is ${celsiusValue} a very hot day!`;
+  } else {
+    alert("Check inputs something went wrong!");
+  }
+  return message;
+};
+
+const finalWeatherApp = describeTemperature();
+
+alert(finalWeatherApp);
